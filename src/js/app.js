aec55e645c4dc4cb56401050d704d2a7d604c098
Roman Marchenko <kirkusik@gmail.com>
2023-09-07 16:55:20 +0300

    finalle

--- src/js/app.js
+++ src/js/app.js
@@ -22,8 +22,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	const menuBtn = document.querySelector(".menu-toggle");
 
-	menuBtn.addEventListener("click", () => {
+	menuBtn.addEventListener("click", function menuClickHandler() {
 		const menu = document.querySelector(".menu");
+
 		menu.classList.toggle("menu--active");
+		document.body.classList.toggle("page__body--w-overflow");
+		menuBtn
+			.querySelector(".menu-toggle__icon")
+			.classList.toggle("menu-toggle__icon--active");
+
+		menu.addEventListener("click", function menuLinkHandler(e) {
+			document.body.classList.toggle("page__body--w-overflow");
+			menu.classList.toggle("menu--active");
+			menuBtn
+				.querySelector(".menu-toggle__icon")
+				.classList.toggle("menu-toggle__icon--active");
+
+			menu.removeEventListener("click", menuLinkHandler);
+		});
+
+		// menuBtn.removeEventListener("click", menuClickHandler);
 	});
 });
