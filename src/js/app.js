2978966242b4d4a63442146d627a52b8c3324ba3
Roman Marchenko <kirkusik@gmail.com>
2023-09-29 15:12:20 +0300

    fn

--- src/js/app.js
+++ src/js/app.js
@@ -28,22 +28,27 @@ document.addEventListener("DOMContentLoaded", () => {
 	const menuBtn = document.querySelector(".menu-toggle");
 
 	menuBtn.addEventListener("click", function menuClickHandler() {
-		const menu = document.querySelector(".menu");
+		const menuWrapper = document.querySelector(".menu-wrapper");
+		const menu = document.querySelector(".page__menu");
 
-		menu.classList.toggle("menu--active");
+		menuWrapper.classList.toggle("menu-wrapper--active");
 		document.body.classList.toggle("page__body--w-overflow");
 		menuBtn
 			.querySelector(".menu-toggle__icon")
 			.classList.toggle("menu-toggle__icon--active");
 
 		menu.addEventListener("click", function menuLinkHandler(e) {
-			document.body.classList.toggle("page__body--w-overflow");
-			menu.classList.toggle("menu--active");
-			menuBtn
-				.querySelector(".menu-toggle__icon")
-				.classList.toggle("menu-toggle__icon--active");
+			if (e.target.closest(".page__menu a")) {
+				console.log(e.target);
 
-			menu.removeEventListener("click", menuLinkHandler);
+				document.body.classList.toggle("page__body--w-overflow");
+				menuWrapper.classList.toggle("menu-wrapper--active");
+				menuBtn
+					.querySelector(".menu-toggle__icon")
+					.classList.toggle("menu-toggle__icon--active");
+
+				menu.removeEventListener("click", menuLinkHandler);
+			}
 		});
 
 		// menuBtn.removeEventListener("click", menuClickHandler);
